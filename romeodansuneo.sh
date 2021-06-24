960a1ecdb4113d6f510bb01924ac2b4d49cf2693
asamjawas <85292358+asamjawas@users.noreply.github.com>
2021-06-25 05:06:54 +0700

    Update romeodansuneo.sh

--- romeodansuneo.sh
+++ romeodansuneo.sh
@@ -3,7 +3,7 @@
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
  
-TOKEN="6564fa213079092d969bd2cd603102168a66f91ca3269f8b4f" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+TOKEN="a1836a5b5a66fe38c4babe233a0fb083cb1dbe6ad53909dc60" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
 ~/.buildkite-agent/bin/buildkite-agent start
  
      echo COUNTER $COUNTER
