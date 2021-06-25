18c806a0a2eb7e3e2a0ed7c61262cc5ef8f86720
asamjawas <85292358+asamjawas@users.noreply.github.com>
2021-06-25 18:04:11 +0700

    Update tehpucuk.sh

--- tehpucuk.sh
+++ tehpucuk.sh
@@ -3,7 +3,7 @@
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
  
-TOKEN="17a491300ae24641bb6c67194d0fd8e19d5d0e49f77c9c9eb4" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+TOKEN="af0defee4df70220daa539f4e68945574bd26b0d588e8616ca" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
 ~/.buildkite-agent/bin/buildkite-agent start
  
      echo COUNTER $COUNTER
